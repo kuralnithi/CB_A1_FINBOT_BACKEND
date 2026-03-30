4468555ed447a43462ec149b1bf86376e563184e
Kuralnithi <kuralnithi1999@gmail.com>
2026-03-30 13:11:52 +0530

    empty cmt

--- main.py
+++ main.py
@@ -97,3 +97,4 @@ if __name__ == "__main__":
     if sys.platform == 'win32':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, loop="asyncio")
+ 
